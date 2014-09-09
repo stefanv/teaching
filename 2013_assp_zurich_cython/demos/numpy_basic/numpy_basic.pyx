5e2dfef05fab1bc8dad533acbebeccff5c60de38
Stefan van der Walt <stefan@sun.ac.za>
2014-09-09 09:38:18 +0100

    In basic Cython/numpy example, operate in-memory

--- 2013_assp_zurich_cython/demos/numpy_basic/numpy_basic.pyx
+++ 2013_assp_zurich_cython/demos/numpy_basic/numpy_basic.pyx
@@ -1,13 +1,11 @@
-cimport numpy as cnp
 import numpy as np
 
 def foo(double[:, ::1] arr):
 
     cdef Py_ssize_t i, j
-    cdef double[:, ::1] out = np.zeros_like(arr)
 
     for i in range(arr.shape[0]):
         for j in range(arr.shape[1]):
-            out[i, j] = arr[i, j] + 10
+            arr[i, j] = arr[i, j] + 10
 
-    return np.asarray(out)
+    return np.asarray(arr)
